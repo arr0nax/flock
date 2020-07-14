fa932a844e4fc60506f2cf6816cd04b8335da8cd
clayton <clayton@trilliumtransit.com>
2020-07-13 19:32:23 -0700

    catch errors in api

--- api/src/services/topics.js
+++ api/src/services/topics.js
@@ -23,25 +23,30 @@ class TopicService {
   }
 
   async chooseTopic() {
-    const group = await Group.findByID(1);
-    const topics = await group.getTopics();
-
-    const users = await group.getUsersWhoLoggedInToday();
-    // const users = await group.getUsers();
-    // console.log('---------------------users-------------------');
-    // console.log(users);
-    // console.log('---------------------users-------------------');
-
-    var topic = topics.models[Math.floor(Math.random()*topics.length)];
-    var user = users.models[Math.floor(Math.random()*users.length)];
-    console.log(user);
-    const newgroup = await Group.updateById(1, {
-      topic_chosen: false,
-      topic_choser_id: user.id
-    });
-    console.log(newgroup);
-    await User.where({logged_in_today: true}).save({logged_in_today: false}, {method: 'update', patch:true})
-    return topic;
+    try {
+      const group = await Group.findByID(1);
+      const topics = await group.getTopics();
+  
+      const users = await group.getUsersWhoLoggedInToday();
+      // const users = await group.getUsers();
+      // console.log('---------------------users-------------------');
+      // console.log(users);
+      // console.log('---------------------users-------------------');
+  
+      var topic = topics.models[Math.floor(Math.random()*topics.length)];
+      var user = users.models[Math.floor(Math.random()*users.length)];
+      console.log(user);
+      const newgroup = await Group.updateById(1, {
+        topic_chosen: false,
+        topic_choser_id: user && user.id
+      });
+      console.log(newgroup);
+      await User.where({logged_in_today: true}).save({logged_in_today: false}, {method: 'update', patch:true})
+      return topic;
+    } catch (err) {
+      console.log(err);
+      return Boom.forbidden(err.message);
+    }
   }
 }
 
