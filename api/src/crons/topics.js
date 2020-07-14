abee20e0308f456c6529fa0ba4191d38ed186192
clayton <clayton@trilliumtransit.com>
2020-07-13 19:35:47 -0700

    new topic at 6, cron doesnt run twice

--- api/src/crons/topics.js
+++ api/src/crons/topics.js
@@ -2,8 +2,8 @@ import topicService from '../services/topics';
 var CronJob = require('cron').CronJob;
 
 export default () => {
-  new CronJob('0 0 2 * * *', function() {
+  new CronJob('0 0 6 * * *', function() {
     console.log('hello cronjob!');
     topicService.chooseTopic();
-  }, null, true, 'America/Los_Angeles', this, true);
+  }, null, true, 'America/Los_Angeles', this, false);
 }
