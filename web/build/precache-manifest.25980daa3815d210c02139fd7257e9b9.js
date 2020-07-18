e4e8405f14e5cab6f948998039a46592f5441e68
clayton <clayton@trilliumtransit.com>
2020-07-18 12:46:47 -0700

    try using build

---
+++ web/build/precache-manifest.25980daa3815d210c02139fd7257e9b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b4d8c13464fb4616c74a60b9563bcd8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8aba150c3a94aa2226e9",
+    "url": "./static/css/main.c0256671.chunk.css"
+  },
+  {
+    "revision": "20121762f9507e7ea616",
+    "url": "./static/js/2.d86da157.chunk.js"
+  },
+  {
+    "revision": "8d23d1a2df5781c36b781bca70939de9",
+    "url": "./static/js/2.d86da157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aba150c3a94aa2226e9",
+    "url": "./static/js/main.eae1884a.chunk.js"
+  },
+  {
+    "revision": "ab2926fcd9fa8f7f9027",
+    "url": "./static/js/runtime-main.5ac40726.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dfef9f40309d501fd02eb7f90702eae7",
+    "url": "./static/media/smallsheepboi.dfef9f40.png"
+  }
+]);
